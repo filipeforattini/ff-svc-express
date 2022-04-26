a5d62f86df84c09ea2704e897012e5e236dc944b
Filipe Forattini <filipe@vitta.me>
2022-04-26 16:33:48 -0300

    chore: new code addition

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const dayjs = require('dayjs');
+const dayjs = require("dayjs");
+const chalk = require("chalk");
+
+const colors = [
+  "black",
+  "red",
+  "green",
+  "yellow",
+  "blue",
+  "magenta",
+  "cyan",
+  "white",
+  "gray",
+  "redBright",
+  "greenBright",
+  "yellowBright",
+  "blueBright",
+  "magentaBright",
+  "cyanBright",
+  "whiteBright",
+];
 
 setInterval(() => {
-  console.log(`   T1M3 :: ${dayjs().format('HH:mm:ss')}`)
-}, 2 * 1000)
+  console.log(
+    chalk[colors[parseInt(Math.random() * 100) % colors.length]](
+      `    T1M3 :: ${dayjs().format("HH:mm:ss")}`
+    )
+  );
+}, 2 * 1000);
