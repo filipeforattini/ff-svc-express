b8c099514b0e3996d26ed0a2e6eb7dbd985b8cba
Filipe Forattini <filipe@vitta.me>
2022-04-27 09:37:11 -0300

    feat(module): added support for env vars BREAKING CHANGE: Now the interval must be integer.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import dayjs from "dayjs"
-import { randomColorLog } from './colors'
+import dayjs from "dayjs";
+import { randomColorLog } from "./colors";
+
+const { PRINT_INTERVAL = 2 * 1000 } = process.env;
 
 setInterval(() => {
-  randomColorLog(`    T1M3 :: ${dayjs().format("HH:mm:ss")}`)
-}, 2 * 1000);
+  randomColorLog(`    T1M3 :: ${dayjs().format("HH:mm:ss")}`);
+}, parseInt(PRINT_INTERVAL));
