700e2bc076af2c4d00ef1079e9fd7c56e2b7f3a8
Filipe Forattini <filipe@vitta.me>
2022-04-29 10:08:36 -0300

    feat: rewrote color definition

--- src/main.js
+++ src/main.js
@@ -5,17 +5,16 @@ import express from "express";
 import favicon from "serve-favicon";
 import bodyParser from "body-parser";
 import compression from "compression";
-
 const morgan = require("morgan");
-const { name, version } = require("../package.json");
 
+const { name, version } = require("../package.json");
 import { randomColor, coloredLog } from "./colors";
 
 const { PORT = "8080" } = process.env;
 
 const app = express();
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 const middlewares = [
   morgan("dev"),
@@ -30,16 +29,20 @@ const middlewares = [
 middlewares.filter((m) => app.use(m));
 
 let count = 0;
-app.get('/', (req, res) => {
-  const color = randomColor()
-  coloredLog(color, 'Hello world!')
+let color = randomColor();
 
-  res.render('home', {
+setInterval(() => {
+  color = randomColor()
+  coloredLog(color, "Current color: " + color);
+}, 2 * 1000);
+
+app.get("/", (req, res) => {
+  res.render("home", {
     name,
     color,
     version,
     count: count++,
-    link: 'https://github.com/filipeforattini/ff-svc-pipetest',
+    link: "https://github.com/filipeforattini/ff-svc-pipetest",
   });
 });
 
