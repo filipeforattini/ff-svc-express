672a4b0d6e9a35236c664246903f83ff8f61b9ab
Filipe Forattini <filipe@vitta.me>
2022-05-12 10:12:40 -0300

    fix: added env prefix at start

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
-import expressFactory from './express'
+import expressFactory from "./express";
 import { randomColor, coloredLog } from "./colors";
 
-const { PORT = "8080" } = process.env;
 const { name, version } = require("../package.json");
 
-const app = expressFactory()
+const { 
+  NODE_ENV, 
+  PORT = "8080" 
+} = process.env;
+
+const app = expressFactory();
 
 let count = 0;
 let color = randomColor();
 
 setInterval(() => {
-  color = randomColor()
+  color = randomColor();
   coloredLog(color, "Current color: " + color);
 }, 2 * 1000);
 
@@ -24,4 +28,6 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(parseInt(PORT), () => console.log(`Listening on port ${PORT}`));
+app.listen(parseInt(PORT), () =>
+  console.log(`${NODE_ENV || '' }   Listening on port ${PORT}`)
+);
